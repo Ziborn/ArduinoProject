3c26ef19ecd81a4eecfa562c74a1b65519cbeda9
Ziborn <robin.mik23@gmail.com>
2018-01-30 13:47:20 +0100

    Add files via upload

---
+++ AdruinoProject/AdruinoProject/Controllers/EmailController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AdruinoProject.Data;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AdruinoProject.Controllers
+{
+    [Route("api/email")]
+    public class EmailController : Controller
+    {
+        private readonly DataManager dataManager;
+
+        public EmailController(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
+
+        [Route("sendEmail")]
+        public IActionResult SendEmail()
+        {
+            dataManager.SendEMail();
+            return Ok("Mail sent!");
+        }
+    }
+}
---
+++ AdruinoProject/AdruinoProject/Data/DataManager.cs
@@ -0,0 +1,48 @@
+﻿using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MailKit.Net.Smtp;
+using MailKit;
+
+namespace AdruinoProject.Data
+{
+    public class DataManager
+    {
+        public void SendEMail()
+        {
+            string date = DateTime.Now.ToShortDateString();
+            string time = DateTime.Now.ToShortTimeString();
+
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Security alarm", "love.wick93@gmail.com"));
+            message.To.Add(new MailboxAddress("Robin", "robin.mik23@gmail.com"));
+            message.Subject = "Security breach";
+
+            message.Body = new TextPart("html")
+            {
+                
+                Text = @"<b>Hi Robin,</b><br>
+
+<p>Someone just <span style='color:red'>triggered</span> your alarm. " + date + " " + time + "</p>"
+            };
+
+
+
+            using (var client = new SmtpClient())
+            {
+                // For demo-purposes, accept all SSL certificates (in case the server supports STARTTLS)
+                client.ServerCertificateValidationCallback = (s, c, h, e) => true;
+
+                client.Connect("smtp.gmail.com", 587, false);
+
+                // Note: only needed if the SMTP server requires authentication
+                client.Authenticate("love.wick93@gmail.com", "lgaoyrxmdragdqfr");
+
+                client.Send(message);
+                client.Disconnect(true);
+            }
+        }
+    }
+}
